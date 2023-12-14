2f6327e88a76b2a0f8de76b8f940b1c6e81a7300
nagasai305 <nagasai30517@gmail.com>
2023-12-14 18:29:02 +0530

    variables5

--- 05.Variables.sh
+++ 05.Variables.sh
@@ -10,3 +10,10 @@ echo "Please enter your username: "
 read -s USERNAME
 
 echo "Username is: $USERNAME"
+
+echo "Please enter your password: "
+
+read -s PASSWORD
+
+echo "Password is: $PASSWORD"
+
