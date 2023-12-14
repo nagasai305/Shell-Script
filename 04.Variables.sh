82d34a6a7540e77738d51293cd321f8b3808150a
nagasai305 <nagasai30517@gmail.com>
2023-12-14 18:09:27 +0530

    variables2

---
+++ 04.Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hello $PERSON2, Good Morning"
+echo "$PERSON2 : Hello $PERSON1, Good Morning"
+echo "$PERSON1 : How are you ?"
+echo "$PERSON2 : I am fine"
