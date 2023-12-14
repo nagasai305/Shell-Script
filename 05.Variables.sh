a18152b5d7270f0ffc461421571e40cd53e35c6f
nagasai305 <nagasai30517@gmail.com>
2023-12-14 18:25:27 +0530

    variables5

--- 05.Variables.sh
+++ 05.Variables.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+ #USERNAME=$1
+#PASSWORD=$2
 
-echo "Username is: $USERNAME, Password is: $PASSWORD"
+ #echo "Username is: $USERNAME, Password is: $PASSWORD"
+
+echo "Please enter your username: "
+
+read -s USERNAME
+
+echo "Username is: $USERNAME"
