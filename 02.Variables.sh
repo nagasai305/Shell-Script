8815320286a15aaf032bdf0ece4ec4c6b05b512f
nagasai305 <nagasai30517@gmail.com>
2023-12-14 17:40:17 +0530

    variable

--- 02.Variables.sh
+++ 02.Variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Nowroj:: Hello sai, Good Morning"
-echo "sai :: Hello Nowroj, Good Morning"
-echo "Noworoj :: How are you ?"
-echo "Sai:: I am fine"
+PERSON1=Nowroj
+PERSON2=Sai
+
+echo "$PERSON1:: Hello $PERSON2, Good Morning"
+echo "$PERSON2 :: Hello $PERSON1, Good Morning"
+echo "$PERSON1 :: How are you ?"
+echo "$PERSON2:: I am fine"
